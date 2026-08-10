423ecdfb9098d9500264572abd703cd4504e25f6
Mats Rynge <mats@rynge.net>
2026-08-10 08:57:39 -0700

    Added Matlab 2026a

---
+++ htc/matlab-runtime:R2026a/90-osgvo-matlab-runtime.sh
@@ -0,0 +1,4 @@
+
+LD_LIBRARY_PATH=/usr/local/MATLAB/MATLAB_Runtime/v99/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v99/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v99/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v99/extern/bin/glnxa64:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH
+
